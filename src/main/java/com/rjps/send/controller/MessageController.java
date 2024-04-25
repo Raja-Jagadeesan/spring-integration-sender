91179b381f69cd02afefa8f2e6fa388a16463a51
Raja Jagadeesan <raja_jagadeesan@ymail.com>
2024-04-26 02:05:21 +0530

    Update MessageController.java

--- src/main/java/com/rjps/send/controller/MessageController.java
+++ src/main/java/com/rjps/send/controller/MessageController.java
@@ -3,6 +3,8 @@
  */
 package com.rjps.send.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +21,30 @@ import com.rjps.send.SpringIntegrationSenderApplication.PubsubOutboundGateway;
 @RequestMapping(path = "/rjps")
 public class MessageController {
 
+	public static final Logger logger = LogManager.getLogger(MessageController.class);
+
 	@Autowired
 	private PubsubOutboundGateway messagingGateway;
 
 	@PostMapping("/postMessage")
 	public RedirectView postMessage(@RequestParam("message") String message) {
 		this.messagingGateway.sendToPubsub(message);
+		return new RedirectView("/");
+	}
+
+	@PostMapping("/postMessageLoop")
+	public RedirectView postMessage(@RequestParam("message") String message, @RequestParam("count") int count,
+			@RequestParam("interval") int interval) {
+		for (int i = 0; i < count; i++) {
+			this.messagingGateway.sendToPubsub(i + ". " + message);
+			logger.info("Message sent! Payload: {}", i + ". " + message);
+			try {
+				Thread.sleep(interval * 1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		return new RedirectView("/");
 	}
 }
